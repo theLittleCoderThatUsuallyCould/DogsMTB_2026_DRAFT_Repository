b189ab94b0dadd1a89aa724ca052cbdeb0ced2b8
MJ <m.jenkins.dick@gmail.com>
2026-03-03 21:25:11 -0500

    fixed menu size, can click to exit, and pictures going over it. Changed font size using clamp(). Changed the wording on the JOIN page.

--- DogsMTB.js
+++ DogsMTB.js
@@ -1,10 +1,26 @@
 const hamMenu = document.querySelector(".ham-menu");
-
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+//Opens the hamburger menu
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+});
+
+//Closes the hamburger menu when clicked outside the menu
+document.addEventListener('click', function(event) {
+  // Check if the menu is open
+
+  // AND check if the click was outside the menu
+  // AND check if the click was not on the button itself
+  if (!hamMenu.contains(event.target) && !offScreenMenu.contains(event.target)) {
+    // Remove the active class to close the menu
+    if(offScreenMenu.classList.contains("active")){
+    offScreenMenu.classList.toggle('active');
+    hamMenu.classList.toggle("active");
+    // If you have a separate toggle icon, you can turn it off here too
+    // menuButton.classList.remove('is-active');
+  }}
 });
 
 //FAQ
